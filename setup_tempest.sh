f3701978b19fc2f0f3af2fbfb35612385fddad64
Oleksiy Butenko <obutenko@mirantis.com>
2017-02-09 18:19:11 +0200

    Update setup_tempest.sh

--- setup_tempest.sh
+++ setup_tempest.sh
@@ -5,5 +5,5 @@ source /home/root/openrc
 rally-manage db recreate
 rally deployment create --fromenv --name=tempest
 rally verify create-verifier --type tempest --name tempest-verifier --source /var/lib/tempest --version 14.0.0 --system-wide
-rally verify configure-verifier --extend options.conf
+rally verify configure-verifier --extend /home/root/options.conf
 rally verify configure-verifier --show
