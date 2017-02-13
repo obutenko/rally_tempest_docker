02639d421b570f124011bc0b5ca2a5ec08f297b1
Oleksiy Butenko <obutenko@mirantis.com>
2017-02-13 11:07:29 +0200

    Update setup_tempest.sh

--- setup_tempest.sh
+++ setup_tempest.sh
@@ -5,5 +5,5 @@ source /home/root/keystonercv3
 rally-manage db recreate
 rally deployment create --fromenv --name=tempest
 rally verify create-verifier --type tempest --name tempest-verifier --source /var/lib/tempest --version 14.0.0 --system-wide
-rally verify configure-verifier --extend /home/root/options.conf
+rally verify configure-verifier --extend /root/options.conf
 rally verify configure-verifier --show
