46440b5ddb9b623346cc76dc6b24392262714652
obutenko <obutenko@mirantis.com>
2017-03-02 12:59:12 +0200

    Fix typo

--- run_tempest.sh
+++ run_tempest.sh
@@ -5,7 +5,8 @@ source /home/rally/keystonercv3
 rally-manage db recreate
 rally deployment create --fromenv --name=tempest
 rally verify create-verifier --type tempest --name tempest-verifier --source /var/lib/tempest --version 14.0.0 --system-wide
-rally verify configure-verifier --extend /var/lib/tempest_conf/$temest_conf
+rally verify configure-verifier --extend /var/lib/tempest_conf/$TEMPEST_CONF
+rally verify start --skip-list /var/lib/skip-list 
 rally verify configure-verifier --show
-rally verify start --skip-list /var/lib/skip_lists/$skip_list
+rally verify start --skip-list /var/lib/skip_lists/$SKIP_LIST
 rally verify report --type junit-xml --to report.xml
