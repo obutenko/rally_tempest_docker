59302752b02a563e354db4f8bd8baaf35e571bcd
Oleksii Butenko <apbutenko@gmail.com>
2018-03-19 16:46:07 +0200

    Update run_tempest.sh

--- run_tempest.sh
+++ run_tempest.sh
@@ -4,8 +4,9 @@ source /home/rally/$SOURCE_FILE
 
 rally-manage db recreate
 rally deployment create --fromenv --name=tempest
-rally verify create-verifier --type tempest --name tempest-verifier --source /var/lib/tempest --version 15.0.0 --system-wide
-rally verify configure-verifier --extend /var/lib/tempest_conf/$TEMPEST_CONF
+rally verify create-verifier --type tempest --name tempest-verifier --source /var/lib/tempest --version 16.0.0 --system-wide
+rally verify configure-verifier --extend /var/lib/$TEMPEST_CONF
 rally verify configure-verifier --show
-rally verify start --skip-list /var/lib/skip_lists/$SKIP_LIST $CUSTOM
+rally verify start $CUSTOM
 rally verify report --type junit-xml --to report.xml
+rally verify report --type html --to report.html
